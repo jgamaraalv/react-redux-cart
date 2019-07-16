5ee3b29532d81e2e1003ddfa3d19212e3eaf613b
João Guilherme do Amaral Vequiato <guilive13@gmail.com>
2019-07-16 19:21:17 -0300

    list products in the cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,7 +20,8 @@ function Header({ cartSize }) {
     </Container>
   );
 }
-
-export default connect(state => ({
+const mapStateToProps = state => ({
   cartSize: state.cart.length,
-}))(Header);
+});
+
+export default connect(mapStateToProps)(Header);
